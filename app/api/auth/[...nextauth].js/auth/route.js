f7f6db6ef0b8d68c1aeaddbc14c30454df2d1c8a
Vishwajeet <vishwajeetmahorevm@gmail.com>
2025-03-28 21:15:45 +0530

    Update route.js

--- app/api/auth/[...nextauth].js/auth/route.js
+++ app/api/auth/[...nextauth].js/auth/route.js
@@ -10,7 +10,7 @@ import User from '@/models/User';
 import Payment from '@/models/Payment';
  
 
-export const authoptions =  NextAuth({
+export const dynamic =  NextAuth({
     providers: [
       // OAuth authentication providers...
       GitHubProvider({
